b515c0e35c975cf02e519a0581ca033669cfcd65
d022361 <dirk.feeken@sap.com>
2016-02-07 14:58:24 +0000

    banner removed

--- localization.js
+++ localization.js
@@ -29,7 +29,7 @@ String.toLocaleString({
 		"%tt_units": "choose between SI (meter, km/h) and US customary (inch, mile) units",
 		"%tt_url": "use this URL to point to the currently selected gearing e.g. for referencing it in online discussions etc.",
 		"%tt_chain_angle": "sets the maximum allowed chain angle (approx. in degrees). Combinations where the chain runs too diagonal and should not be used are greyed out in the graphics.",
-		"%ribbontext_top": "Sheldon Brown",
+		"%ribbontext_top": "",
 		"%ribbontext_sub": "7/14/1944 - 2/4/2008",
 		"%ribbon_link": "https://en.wikipedia.org/wiki/Sheldon_Brown_(bicycle_mechanic)"
 	},
@@ -131,7 +131,7 @@ String.toLocaleString({
 		"%tt_units": "Auswahl zwischen SI (meter, km/h) und US (inch, mile) Einheiten",
 		"%tt_url": "Diese URL zeigt direkt die gerade ausgewählte Schaltung an und kann z.B. als Referenz in Online Diskussionen verwendet werden",
 		"%tt_chain_angle": "Setzt den maximal erlaubten Kettenschräglauf (näherungsweise in Grad). Kombinationen, bei denen die Kette schräger läuft als dieser Wert, werden in der Grafik ausgegraut.",
-		"%ribbontext_top": "Sheldon Brown",
+		"%ribbontext_top": "",
 		"%ribbontext_sub": "14.7.1944 - 4.2.2008",
 		"%ribbon_link": "https://en.wikipedia.org/wiki/Sheldon_Brown_(bicycle_mechanic)"
 	},
