a858e95ab0c400c493d3e2718391399e34dcaf31
ProUkorNew <proukornevv@gmail.com>
2017-08-05 14:52:45 +0300

    2

--- graphics.js
+++ graphics.js
@@ -165,14 +165,14 @@ function drawGraphics(canvas, gearSet, minDev, maxDev, cadence, dsplOps) {
 				console.log(gearSet.Chainrings.length);
 				if (gearSet.Chainrings.length == 3) {
 					console.log("test");
-					var diff_ratio_up = (gearSet.Chainrings[i] / gearSet.Cogs[j-1]) - (gearSet.Chainrings[i] / gearSet.Cogs[j]);
-					var diff_ratio_down = (gearSet.Chainrings[i] / gearSet.Cogs[j]) - (gearSet.Chainrings[i] / gearSet.Cogs[j+1]);
+					var diff_ratio_1 = (gearSet.Chainrings[i] / gearSet.Cogs[j-1]) - (gearSet.Chainrings[i] / gearSet.Cogs[j]);
+					var diff_ratio_2 = (gearSet.Chainrings[i] / gearSet.Cogs[j]) - (gearSet.Chainrings[i] / gearSet.Cogs[j+1]);
 					var ratio_up_next = [];
 					var ratio_down_next = [];
 					if (i == 0 || i == 1) {
 						for (var k = 0; k < 4; k++) {
 							ratio_up_next.push(gearSet.Chainrings[i+1] / gearSet.Cogs[j+k]);
-							if (ratio_up_next[k] - (gearSet.Chainrings[i] / gearSet.Cogs[j]) < diff_ratio_up) {
+							if (ratio_up_next[k] - (gearSet.Chainrings[i] / gearSet.Cogs[j]) < diff_ratio_2) {
 								y_d = Math.round(gHeight / (gearSet.Chainrings.length + 1) * (i + 2)) + gY -10.5;
 								x_d = gX + Math.round(xLog(minDev, maxDev, gWidth, gearSet.Chainrings[i+1] / gearSet.Cogs[j+k] * gearSet.circumference / 1000));
 							}
